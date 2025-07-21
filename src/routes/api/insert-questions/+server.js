4eb0f93fefb8d61c7208574b7602fd3963c77ed7
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2025-07-21 11:35:18 +0300

    Tweaked the question generation prompt

--- src/routes/api/insert-questions/+server.js
+++ src/routes/api/insert-questions/+server.js
@@ -69,6 +69,7 @@ export const POST = async ({ fetch, request }) => {
         Things not to do:
         1. Do not include the number of a step when following the explanation framework
         2. Do not change the question body, or answers
+        3. Do not change the correct answer
 
         Follow this JSON structure as shown in the example:
 
